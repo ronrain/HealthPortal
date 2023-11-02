9820a07b3ca71c8f17e807fc658422671e00dff9
RonRain <rarainero@gmail.com>
2023-11-02 15:06:40 -0400

    add patient model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+  name = models.CharField(max_length=100)
+  gender = models.CharField(max_length=100)
+  race = models.CharField(max_length=100)
+  birth_date = models.DateField()
+  medical_history = models.TextField(max_length=250)
+  description = models.TextField(max_length=250)
--- main_app/views.py
+++ main_app/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 
 def home(request):
-  return HttpResponse('<h1>HPHP</h1>')
+  return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
