3701e7c4d9cb4ef4ea255fb0eac9b210abfcf617
RonRain <rarainero@gmail.com>
2023-11-03 09:32:26 -0400

    add visits model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.urls import reverse
 
+OPTIONS = (
+  ('Y', 'Yes'),
+  ('N', 'No')
+)
+
 class Patient(models.Model):
   name = models.CharField(max_length=100)
   gender = models.CharField(max_length=100)
@@ -14,3 +19,21 @@ class Patient(models.Model):
   
   def get_absolute_url(self):
     return reverse('patient-detail', kwargs={'patient_id': self.id})
+  
+class Visits(models.Model):
+  visit_type = models.CharField(max_length=100)
+  date = models.DateField('Visit date')
+  height = models.IntegerField()
+  weight = models.IntegerField()
+  diagnosis = models.CharField(max_length=100)
+  treatment = models.TextField(max_length=250)
+  again = models.CharField(
+    max_length=1,
+    choices=OPTIONS,
+    default=OPTIONS[1][1]
+  )
+
+  patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_visit_display()} on {self.date}"
